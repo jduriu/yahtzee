fba83b3140cccd38d40f7c002f7b1df11f57e064
jduriu <jduriu@gmail.com>
2024-02-27 09:52:49 -0800

    add links between login and signup pages + refactor form field jsx to map over a list

--- yahtzee-front/app/(auth)/_components/LoginForm.tsx
+++ yahtzee-front/app/(auth)/_components/LoginForm.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react"
 import { TokenAuth } from "@/utils/authUtils"
+import Link from "next/link"
 
 
 export default function LoginForm() {
@@ -15,22 +16,34 @@ export default function LoginForm() {
     await login(username, password)
   }
 
+  const formFields = [
+    {name: "Username", value: username, changeHandler: setUsername},
+    {name: "Password", value: password, changeHandler: setPassword},
+  ]
+
+
   return (
-    <div className="flex flex-col h-full w-full bg-white/60 shadow-2xl rounded-3xl px-10 py-5 justify-center items-center">
-      <div className="text-3xl h-1/4 w-full flex justify-center items-center">Login Form</div>
-      <div className="flex h-1/2 w-full flex-col gap-3 justify-center items-center">
-        <div className="">Username:</div>
-        <input className="bg-white w-2/3 p-2" type="text" value={username} onChange={(e) => setUsername(e.target.value)}/>
-        <div className="">Password:</div>
-        <input className="bg-white w-2/3 p-2" type="text" value={password} onChange={(e) => setPassword(e.target.value)}/>
+    <div className="flex flex-col gap-10 h-full w-full bg-white/60 shadow-2xl rounded-3xl px-10 py-5 justify-center items-center">
+      <div className="text-3xl w-full flex justify-center items-center">Login Form</div>
+      <div className="flex w-full flex-col gap-3 justify-center items-center">
+        {formFields.map((field) => (
+          <>
+            <div className="">{field.name}:</div>
+            <input className="bg-white w-2/3 p-2" type="text" value={field.value} onChange={(e) => field.changeHandler(e.target.value)}/>
+          </>
+        ))}
       </div>
-      <div className="h-1/4 w-full flex justify-center items-center">
+      <div className="w-full flex flex-col gap-10 justify-center items-center">
         <button
-          className="px-10 py-2 bg-blue-500 border-4 border-white rounded-full hover:bg-gray-300 hover:border-black"
+          className="px-10 py-2  bg-blue-500 border-4 border-white rounded-full hover:bg-gray-300 hover:border-black"
           onClick={handleSubmit}
         >
           Submit
         </button>
+        <div className="text-gray-500">
+          <span>If you do not have an account, sign-up by clicking </span>
+          <Link href="/signup" className="text-blue-500 hover:underline">here</Link>
+        </div>
       </div>
     </div>
   )
--- yahtzee-front/app/(auth)/_components/SignupForm.tsx
+++ yahtzee-front/app/(auth)/_components/SignupForm.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react"
 import { TokenAuth } from "@/utils/authUtils"
+import Link from "next/link"
 
 
 export default function SignupForm({loading, setLoading, loggingIn, setLoggingIn}) {
@@ -24,26 +25,37 @@ export default function SignupForm({loading, setLoading, loggingIn, setLoggingIn
     const new_user = await signUp(formData)
   }
 
+  const formFields = [
+    {name: "Username", value: username, changeHandler: setUsername},
+    {name: "Password", value: password, changeHandler: setPassword},
+    {name: "Email", value: email, changeHandler: setEmail},
+    {name: "Full Name", value: fullName, changeHandler: setFullName},
+  ]
+
   return (
-    <div className="flex flex-col h-full w-full bg-white/60 shadow-2xl rounded-3xl px-10 py-5 justify-center items-center">
-      <div className="text-3xl h-1/4 w-full flex justify-center items-center">Signup Form</div>
-      <div className="flex h-1/2 w-full flex-col gap-3 justify-center items-center">
-        <div className="">Username:</div>
-        <input className="bg-white w-2/3 p-2" type="text" value={username} onChange={(e) => setUsername(e.target.value)}/>
-        <div className="">Password:</div>
-        <input className="bg-white w-2/3 p-2" type="text" value={password} onChange={(e) => setPassword(e.target.value)}/>
-        <div className="">Email:</div>
-        <input className="bg-white w-2/3 p-2" type="text" value={email} onChange={(e) => setEmail(e.target.value)}/>
-        <div className="">Full Name:</div>
-        <input className="bg-white w-2/3 p-2" type="text" value={fullName} onChange={(e) => setFullName(e.target.value)}/>
+    <div className="flex flex-col h-full w-full bg-white/60 shadow-2xl rounded-3xl px-10 py-5 justify-center items-center gap-10">
+      <div className="text-3xl w-full flex justify-center items-center">Signup Form</div>
+      <div className="flex w-full flex-col gap-3 justify-center items-center">
+        {formFields.map((field) => (
+          <>
+            <div className="">{field.name}:</div>
+            <input className="bg-white w-2/3 p-2" type="text" value={field.value} onChange={(e) => field.changeHandler(e.target.value)}/>
+          </>
+        ))}
+
       </div>
-      <div className="h-1/4 w-full flex justify-center items-center">
+      <div className="w-full justify-center items-center flex flex-col gap-10">
         <button
           className="px-10 py-2 bg-blue-500 border-4 border-white rounded-full hover:bg-gray-300 hover:border-black"
           onClick={handleSubmit}
         >
           Submit
         </button>
+        <div className="text-gray-500">
+          <span>If you already have an account, click </span>
+          <Link href="/login" className="text-blue-500 hover:underline">here</Link>
+          <span> to login</span>
+        </div>
       </div>
     </div>
   )
