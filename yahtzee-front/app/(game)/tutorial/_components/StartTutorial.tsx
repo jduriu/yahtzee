8cd7e0f8990271d990b746f2ab9d04021c665b42
jduriu <jduriu@gmail.com>
2024-04-27 14:54:07 -0700

    Create Tutorial setup   - Setup page to map over component array and load the steps in sequence   - Create StartTutorial component   - Create ForwardBackButtons component

---
+++ yahtzee-front/app/(game)/tutorial/_components/StartTutorial.tsx
@@ -0,0 +1,19 @@
+import React, { SetStateAction } from 'react';
+import Button from '@/global_components/Button';
+
+type StartTutorialProps = {
+  step: number;
+  setStep: React.Dispatch<SetStateAction<number>>
+}
+
+
+const StartTutorial = ({ step, setStep }: StartTutorialProps) => {
+  return (
+    <div className="h-full flex flex-col gap-3 items-center justify-center">
+      <div>Lets Get Started!</div>
+      <Button clickHandler={() => setStep(step + 1)} content="Click Here" style="submit" />
+  </div>
+  )
+};
+
+export default StartTutorial;
--- yahtzee-front/app/(game)/tutorial/page.tsx
+++ yahtzee-front/app/(game)/tutorial/page.tsx
@@ -1,5 +1,37 @@
-export default function Tutorial () {
+'use client'
+
+import Button from '@/global_components/Button';
+import React, { useState } from 'react';
+import ForwardBackButtons from './_components/ForwardBackButtons';
+import StartTutorial from './_components/StartTutorial';
+
+/*
+Step sequence:
+  - Lets get started
+  - Overall layout
+  -
+*/
+
+
+const Tutorial = () => {
+  const [step, setStep] = useState(0)
+
+  const components = [
+    <StartTutorial key={0} step={step} setStep={setStep} />,
+  ]
+
   return (
-    <div>Tutorial</div>
+    <div className="w-full h-full flex flex-col p-5">
+      <div className={step > 0 ? "h-full": "h-[90%]"}>
+        {components.map((component, index) =>
+          index === step && component
+        )}
+      </div>
+      {step > 0 &&
+        <ForwardBackButtons step={step} setStep={setStep} />
+      }
+    </div>
   )
-}
+};
+
+export default Tutorial;
